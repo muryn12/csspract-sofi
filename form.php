7f1ced9fef7017c83e5774e0e6537804f96f1927
Galyna Muryn <broslavska@gmail.com>
2019-03-15 09:49:32 +0000

    Initial

--- form.php
+++ form.php
@@ -1,10 +1,4 @@
 <p>
-    Привіт <?php echo $_POST["ім'я"]; ?><br>
-    Електронна адреса : <?php echo $_POST["email"]; ?>
-</p>
-
-
-<p>
     Hello <?php echo $_POST["name"]; ?><br>
     Your password is: <?php echo $_POST["password"]; ?>
 </p>
